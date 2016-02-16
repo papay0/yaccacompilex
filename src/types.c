ed663f00738b66369ac7a9a537af67bcc8e349fd
Scriptopathe <josue.alvarez@hotmail.fr>
2016-02-16 20:12:24 +0100

    Change types.c memory model

--- src/types.c
+++ src/types.c
@@ -16,38 +16,38 @@ const int PRIM_SIZES[2] = { 1, 4 };
 const primitive_t PRIM_CHAR = 0x00;
 const primitive_t PRIM_INT = 0x01;
 
-type_t type_create_ptr(type_t* type)
+type_t* type_create_ptr(type_t* type)
 {
-	ptrtype_t ptr;
-	ptr.type = type;
-	ptr.kind = TYPE_KIND_POINTER;
-	return (type_t)*((type_t*)&ptr);	 
+	ptrtype_t* ptr = (ptrtype_t*)malloc(sizeof(ptrtype_t));
+	ptr->type = type;
+	ptr->kind = TYPE_KIND_POINTER;
+	return ((type_t*)ptr);	 
 }
 
-type_t type_create_primitive(char* name)
+type_t* type_create_primitive(char* name)
 {
 	primitive_t prim;
-	primtype_t type;
-	type.kind = TYPE_KIND_PRIMITIVE;
+	primtype_t* type = (primtype_t*)malloc(sizeof(primtype_t));
+	type->kind = TYPE_KIND_PRIMITIVE;
 	if(strcmp(name, "char") == 0)
-		type.primitive = PRIM_CHAR;
+		type->primitive = PRIM_CHAR;
 	else if(strcmp(name, "int") == 0)
-		type.primitive = PRIM_INT;
+		type->primitive = PRIM_INT;
 	else
 		assert(0);
-	return (type_t)*((type_t*)&type);
+	return ((type_t*)type);
 }
 
-type_t type_create_func(type_t* return_type, type_t** arg_types, int argc)
+type_t* type_create_func(type_t* return_type, type_t** arg_types, int argc)
 {
-	functype_t func;
-	func.kind = TYPE_KIND_FUNCTION;
-	func.return_type = return_type;
+	functype_t* func = (functype_t*)malloc(sizeof(functype_t));
+	func->kind = TYPE_KIND_FUNCTION;
+	func->return_type = return_type;
 	for(int i = 0; i < argc; i++)
 	{
-		func.arg_types[i] = &arg_types[i];
+		func->arg_types[i] = arg_types[i];
 	}
-	return (type_t)*((type_t*)&func);
+	return ((type_t*)func);
 }
 
 // TODO
