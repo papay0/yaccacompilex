457f231cabae57989ba85e9103df2d52818e15e9
Scriptopathe <josue.alvarez@hotmail.fr>
2016-03-26 12:22:22 +0100

    Ajout des fichiers de test.

---
+++ test/deref.c
@@ -0,0 +1,14 @@
+--
+int main() {
+    int* arr;
+    int a = 0;
+    int *b = &a;
+    int f[64];
+    *b = 4;
+    *b =  arr[4];
+    if(a) {
+        int blbl[64];
+        int c = 255;
+    } 
+    int e = 512;
+}
---
+++ test/table.c
@@ -0,0 +1,30 @@
+int a = 10;
+int b = 20;
+// dé commentair
+--
+int ha(int arg0, int arg1) { a = 256; }
+int main() {
+    a = 55;
+    int e = 56;
+    int d = 45;
+    int e =  = ++ -; // erreur de syntaxe : pas de soucis !
+    /* un commentair multi
+    laïne ! */
+    // utilisation de symboles qui n'existent pas.
+    z = y + &x;
+}
+int hoho(int a, int c)
+{
+    int* g = &a;
+    *g = *g + 5;
+    // ha(777, *g, 10 + 20 * 30, (4+(1+(3+(9)))), 100, 200);
+    ha(g, &g);
+    ha();
+    /* samarch 
+
+    */
+    ha(1, 2);
+    g(56);
+    int tt = 5656;
+}
+
